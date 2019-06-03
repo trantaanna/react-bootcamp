5a0f5a546cbe60b9bfc592f9851ff14cf7c6d28e
Thuy-Anh Tran <ttran@cylance.com>
2019-06-02 21:52:26 -0700

    Adding more project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,32 @@
 import project1 from '../assets/project1.png';
 import project2 from '../assets/project2.png';
 import project3 from '../assets/project3.png';
+import project4 from '../assets/project4.png';
 
 const PROJECTS = [
   {
     id: 1,
     title: 'Example React Application',
     description: 'A React App that I built, involving JS and core web dev concepts!',
-    link: 'https:github.com',
+    link: 'https://github.com/trantaanna/react-bootcamp',
     image: project1
   },
   {
     id: 2,
+    title: 'React TODOList',
+    description: 'React Crash Course tutorial',
+    link: 'https://github.com/trantaanna/React_todolist',
+    image: project4
+  },  
+  {
+    id: 3,
     title: 'Example Websocket TLS using Python',
     description: 'Python example accessing websocket/TLS',
     link: 'https://github.com/trantaanna/websocket_tls',
     image: project2
   },  
   {
-    id: 3,
+    id: 4,
     title: 'Implement Queue using C++',
     description: 'Simple Queue using C++',
     link: 'https://github.com/trantaanna/SimpleQueue',
